9ee90941464c38a819fa8ff7539fb63110d6825b
Ansh <anshnanda10@gmail.com>
2024-10-25 00:12:49 -0500

    0.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { requireNativeModule } from "expo-modules-core";
-
 import { AppEnvironment } from "./ExpoAppEnviroment.types";
+import ExpoAppEnviromentModule from "./ExpoAppEnviromentModule";
 
 // Get the native module
-const NativeModule = requireNativeModule("ExpoAppEnviroment");
 
-export const getEnvironment = (): AppEnvironment => NativeModule.getEnvironment();
-export const environment = NativeModule.environment as AppEnvironment;
+export const getEnvironment = (): AppEnvironment => ExpoAppEnviromentModule.getEnvironment();
+export const environment = ExpoAppEnviromentModule.environment as AppEnvironment;
 
 export default { getEnvironment, environment };
